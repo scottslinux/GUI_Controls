3e4ccb0fbb95eb0069d151cc5d8bb6ae801cc41b
Scott Sher <scottslinux@gmail.com>
2025-07-27 17:25:42 -0700

    Slider Working..needs value tuning

--- Slider.cpp
+++ Slider.cpp
@@ -8,6 +8,7 @@ Texture2D Slider::plate_on{0};
 Texture2D Slider::plate_off{0};
 Texture2D Slider::knob{0};
 
+
 Sound Slider::slide{0};
 Font Slider::pencil{0};
 bool Slider::resourceguard{false};
@@ -22,6 +23,7 @@ Slider::Slider(Vector2 loc, float sliderscale,int detnts,int minim,int maxi)
         plate_on=LoadTexture("./resources/plate_on.png");
         plate_off=LoadTexture("./resources/plate_off.png");
         knob=LoadTexture("./resources/knob.png");
+        pencil=LoadFontEx("./resources/Inter.ttf",100,0,0);
         //slide=LoadSound("./resources/slide");
 
         resourceguard=true; //lock it up!
@@ -68,7 +70,29 @@ void Slider::unloadResources()
 }
 //**************************************************** */
 int Slider::update()
-{
+{   //the 0.8 is to shrink the rectangle around the knob image
+    //knobrect={location.x,location.y,knob.width*scale*.8,knob.height*scale*.8};
+
+    if(CheckCollisionPointRec(GetMousePosition(),knobrect)&& (IsMouseButtonDown(MOUSE_BUTTON_LEFT)))
+    {
+        if(GetMousePosition().x-20>location.x && GetMousePosition().x-20<location.x+plate_off.width*scale*.8)
+        {
+            knobrect.x=GetMousePosition().x-20;
+
+            float percentage=knobrect.x/(location.x+plate_off.width*scale*.8);
+            value=max*percentage;
+
+
+
+
+            
+
+        }
+    }
+
+
+
+
 
     return 0;
 }
@@ -78,10 +102,15 @@ void Slider::draw()
     int offset=200;
     DrawTextureEx(plate_on,location,0,scale,WHITE);
 
-    //DrawRectanglePro(knobrect,{0,0},0,RED);
+    //DrawRectangleLines(knobrect.x,knobrect.y,knob.width*scale*.8,knob.height*scale*.8,GREEN);
 
     DrawTextureEx(knob,{knobrect.x,knobrect.y},0,scale,WHITE);
-    DrawCircle(knobrect.x+(knob.width/2*scale),knobrect.y+knob.height/2*scale,70*scale,BLUE);
+
+    char buffer[50];
+            snprintf(buffer,sizeof(buffer),"Value: %d",value);
+
+            DrawTextEx(pencil,buffer,{location.x,location.y+plate_off.height*scale+50},50,0,BLACK);
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main()
     Button pushbutton({400,400},0.15);
     Button push2({10,10},0.15);
 
-    Slider slide_me({100,900},0.4,5,0,100);
+    Slider slide_me({100,900},0.6,5,0,100);
 
     
     //  Create Test Window for the controls to run in
@@ -36,17 +36,17 @@ int main()
 
 
 
-        ClearBackground(LIGHTGRAY);
+        ClearBackground(Color{92,149,130,255});
 
         pushbutton.update();
         push2.update();
+        slide_me.update();
 
         pushbutton.draw();
         push2.draw();
 
         slide_me.draw();
 
-     
 
 
         EndDrawing();
